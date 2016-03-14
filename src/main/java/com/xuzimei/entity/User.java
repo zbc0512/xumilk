d1bc0f0b814a61c4ee8d68a5848fc19abf11d5a4
ZhangBaicheng <zbc0512@163.com>
2016-03-14 16:23:45 +0800

    目录重构&添加用户实体类

---
+++ src/main/java/com/xuzimei/entity/User.java
@@ -0,0 +1,33 @@
+package com.xuzimei.entity;
+
+public class User {
+
+    private String xu_user_id;
+    private String xu_user_name;
+    private String xu_user_password;
+
+    public String getXu_user_id() {
+        return xu_user_id;
+    }
+
+    public void setXu_user_id(String xu_user_id) {
+        this.xu_user_id = xu_user_id;
+    }
+
+    public String getXu_user_name() {
+        return xu_user_name;
+    }
+
+    public void setXu_user_name(String xu_user_name) {
+        this.xu_user_name = xu_user_name;
+    }
+
+    public String getXu_user_password() {
+        return xu_user_password;
+    }
+
+    public void setXu_user_password(String xu_user_password) {
+        this.xu_user_password = xu_user_password;
+    }
+
+}
