f056360028b75b63f0264f0e228120157b7c0718
ZhangBaicheng <zbc0512@163.com>
2016-03-20 21:07:33 +0800

    登录页面跳转

--- src/main/java/com/xumilk/controller/UserController.java
+++ src/main/java/com/xumilk/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.xumilk.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.xumilk.entity.User;
 
@@ -18,7 +20,13 @@ public class UserController {
 
     @RequestMapping("/login")
     public String login(User user, HttpSession session) {
-        System.out.println("login");
+        return "user/login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login_do(User user, HttpServletRequest request) {
+        String userName = request.getParameter("userName");
+        String userPasswd = request.getParameter("userPasswd");
         return "user/login";
     }
 
